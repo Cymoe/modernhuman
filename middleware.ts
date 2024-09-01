4f20f6b370bbf2617041ca11bbd482a7fa9620a6
Myles Webb <Cymoe@users.noreply.github.com>
2024-09-01 02:36:26 +0200

    trying varable for key

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+console.log("Publishable Key:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+
+export default clerkMiddleware({
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+})
 
 export const config = {
   matcher: [
