2e1fc9db3fd5defc91fa5ad4dc91921a17e96ea5
Myles Webb <Cymoe@users.noreply.github.com>
2024-09-01 02:57:31 +0200

    removed key form middleware

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,7 @@ import { clerkMiddleware } from '@clerk/nextjs/server'
 
 console.log("Publishable Key:", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
 
-export default clerkMiddleware({
-  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-})
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
